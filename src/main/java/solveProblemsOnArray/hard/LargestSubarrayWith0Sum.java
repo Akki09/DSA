49f4b1629da602715061e58ac2443e9b2f835dd1
Akshay Vakharia <Akshayvakharia1995@gmail.com>
2024-05-01 12:32:51 +0530

    DSA-A2Z: Problems on Array[H]: Largest Subarray with 0 Sum updated

--- src/main/java/solveProblemsOnArray/hard/LargestSubarrayWith0Sum.java
+++ src/main/java/solveProblemsOnArray/hard/LargestSubarrayWith0Sum.java
@@ -5,28 +5,33 @@ import java.util.HashMap;
 public class LargestSubarrayWith0Sum {
 
     public static void main(String[] args) {
-        int[] input = new int[]{1, -1, 0, 0, 1};
+        //int[] input = new int[]{1, -1, 0, 0, 1};
+        int[] input = new int[]{19, 0, 32, 49, 21, 25, 23, 31, 20, 5, 50, 49, 9, 44, 10, 13, 3, 31, 48, 22, -1};
         System.out.println(getLongestZeroSumSubarrayLength(input));
     }
 
     public static int getLongestZeroSumSubarrayLength(int[] arr) {
         // Write your code here.
         int sum = 0;
-        int ans = Integer.MIN_VALUE;
-        //Sum and index we will store and duplicate values will be not stored
+
+        //HERE our target is 0 so doing this way
+        int target = 0;
+        int ans = 0;
+
+        // Stor the sum and it's index value for lookup
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
             if (sum == 0) {
-                ans = Math.max(ans, i);
-            } else {
-                if (map.containsKey(-(sum))) {
-                    int temp = map.get(-(sum));
-                    ans = Math.max(ans, i - temp + 1);
-                }
-                if (!map.containsKey(sum)) {
-                    map.put(sum, i);
-                }
+                ans = Math.max(ans, i + 1);
+            }
+            int temp = sum - target;
+            if (map.containsKey(temp)) {
+                int index = map.get(temp);
+                ans = Math.max(ans, i - index);
+            }
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
             }
         }
         return ans;
